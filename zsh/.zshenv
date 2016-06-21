3b1a555c3e8c752cad2f67c16b95ec2a13ad1ae4
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-06-21 16:10:18 +0900

    Revert "test"
    This reverts commit c3e2add36a56fb94c4ccda10fd4a0e383da32abf.

--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,5 +1,3 @@
-export HNAME=`hostname`
-
 export NVM_DIR="/home/usr/member/2013/tonetch/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
--- zsh/zplug_load.zsh
+++ zsh/zplug_load.zsh
@@ -2,8 +2,6 @@ export ZPLUG_HOME=$HOME/.dotfiles/zsh/plugins/.zplug
 source $ZPLUG_HOME/init.zsh
 
 zplug "zplug/zplug"
-zplug "mollifier/cd-gitroot", as:command, use:cd-gitroot  # cd to git root
-zplug 'zsh-users/zsh-syntax-highlighting', nice:10
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main line brackets)
+zplug "mollifier/cd-gitroot"    # cd to git root
 
 zplug load --verbose
