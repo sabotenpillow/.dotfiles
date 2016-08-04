631fce53777be0b357c439e391b51aa8b1b05778
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-08-04 18:37:33 +0900

    install any plugins, for the present

--- zsh/zplug_load.zsh
+++ zsh/zplug_load.zsh
@@ -3,6 +3,10 @@ source $ZPLUG_HOME/init.zsh
 
 zplug "zsh-users/zsh-syntax-highlighting", nice:10
 zplug "b4b4r07/enhancd", use:init.sh
+zplug "b4b4r07/zle-vimode"
+zplug "b4b4r07/zsh-vimode-visual"
+zplug "hchbaw/opp.zsh"
+# zplug "ardagnir/athame"
 
 if ! zplug check; then
   zplug install
