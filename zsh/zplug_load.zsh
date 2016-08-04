40598aca24c15748c22baf3f3fa5bee9028799ae
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-08-04 16:47:10 +0900

    install enhancd

--- zsh/zplug_load.zsh
+++ zsh/zplug_load.zsh
@@ -2,5 +2,14 @@ export ZPLUG_HOME=$HOME/.zplug
 source $ZPLUG_HOME/init.zsh
 
 zplug "zsh-users/zsh-syntax-highlighting", nice:10
+zplug "b4b4r07/enhancd", use:init.sh
+
+if ! zplug check; then
+  zplug install
+fi
 
 zplug load --verbose
+
+if zplug check b4b4r07/enhancd; then
+  export ENHANCD_FILTER=fzf-tmux
+fi
