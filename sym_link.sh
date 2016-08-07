1ad0ce99ac0b89324039b70b40c0a2fa70617e64
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-08-08 01:26:01 +0900

    create .zlogout

--- sym_link.sh
+++ sym_link.sh
@@ -4,6 +4,7 @@ cd $HOME
 ln -fs .dotfiles/zsh/.zshrc
 ln -fs .dotfiles/zsh/.zshenv
 ln -fs .dotfiles/zsh/.zprofile
+ln -fs .dotfiles/zsh/.zlogout
 ln -fs .dotfiles/tmux/.tmux.conf
 ln -fs .dotfiles/tig/.tigrc
 ln -fs .dotfiles/.gemrc
