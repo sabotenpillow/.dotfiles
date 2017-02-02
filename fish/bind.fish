256e91a33adbf1a0ee678a248781853ef9d26e67
sabotenpillow <saboten.pillow@gmail.com>
2017-02-02 12:12:36 +0900

    add any key bind

--- fish/bind.fish
+++ fish/bind.fish
@@ -1,5 +1,7 @@
 function fish_user_key_bindings
   bind \cr history-search-backward
+  bind \cs backward-kill-bigword
+  bind \ei complete-and-search
 
   ## own key bindings
   bind \co __cd-parent-directory
