46e6002b231daf06ad3cc2cf9c09932dc921ab78
sabotenpillow <saboten.pillow@gmail.com>
2017-02-07 21:10:21 +0900

    modify prompt.fish

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -1,38 +1,41 @@
+## ==== color define =======================
+# set user_color         fff # 06bd1f # 09a31e # 6b8e23 # 228b22
+# set host_color         fff # f98ff7
+# set at_color           6b8e23
+# set pwd_color          green # 00a7f4
+# set first_suffix_color fff
+# set status_color       red
+# set open_color         fff # 7804cb
+# set close_color        fff
+
+if not set -q __fish_prompt_hostname
+  set -g __fish_prompt_hostname (hostname)
+end
+
 function fish_prompt
   set exit_code $status
 
-  if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname)
-  end
-
-  ## color define =======================
-  set user_color   06bd1f # 09a31e # 6b8e23 # 228b22
-  set host_color   f98ff7
-  set pwd_color    00a7f4
-  set status_color red
-  set open_color   7804cb
-  set close_color  fff
-
   ## ==== first line =======================
-  echo -n '['
-  set_color $user_color
+  set_color fff
   echo -n $USER
-  set_color normal
+  set_color 6b8e23
   echo -n "@"
-  set_color $host_color
+  set_color fff
   echo -n $__fish_prompt_hostname
   set_color normal
-  echo -n '] '
-  set_color $pwd_color
-  echo (prompt_pwd)
+  echo -n ' '
+  set_color green
+  echo -n (prompt_pwd)
+  set_color fff
+  echo '>'
 
   ## ==== second line =====================
-  set_color $open_color
-  echo -n ":(%"
+  set_color fff
+  echo -n "_"
   if test $exit_code -ne 0
     set_color red
     echo -n ':'$exit_code
   end
-  set_color $close_color
-  echo "): "
+  set_color fff
+  echo ": "
 end
