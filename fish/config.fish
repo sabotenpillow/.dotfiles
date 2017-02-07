1409cb520e3a53ad248b2554cd04f26d29d4e79b
sabotenpillow <saboten.pillow@gmail.com>
2017-02-07 21:39:07 +0900

    RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,17 @@ source $fish_dir/alias.fish
 # source $fish_dir/fish-git/completions/git.fish
 # source $fish_dir/fish-git/functions/prompt_git.fish
 # source $fish_dir/right_prompt.fish
+
+set fish_color_normal         cyan
+set fish_color_command        FF9400
+set fish_color_quote          BF9C30
+set fish_color_redirection    70c0b1
+set fish_color_end            FF4C00
+set fish_color_error          a40000
+set fish_color_param          7aa6da # FFC000
+set fish_color_comment        A63100
+set fish_color_match          --background=brblue
+set fish_color_search_match   bryellow --background=brblack
+set fish_color_operator       bryellow
+set fish_color_escape         bryellow --bold
+set fish_color_autosuggestion 969896
