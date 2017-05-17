43f4ffd27b31df6305eb4afdc02604ac16ddac05
sabotenpillow <saboten.pillow@gmail.com>
2017-05-17 14:19:03 +0900

    fish: set GLOBAL variables

--- fish/environment.fish
+++ fish/environment.fish
@@ -3,19 +3,19 @@ set LANG en_US.UTF-8
 # set EDITOR vim
 
 ## my scripts path
-# [ -d $HOME/.my_scripts ] and set PATH "$HOME/.my_scripts/bin:$PATH"
+# [ -d $HOME/.my_scripts ] and set PATH $HOME/.my_scripts/bin $PATH
 
 ## local path
-# [ -d $HOME/.local ] and set PATH "$HOME/.local/bin:$PATH"
+# [ -d $HOME/.local ] and set PATH $HOME/.local/bin $PATH
 
 ## lpm path
-# [ -d $HOME/local-packages-repository ] and set PATH "$HOME/local-packages-repository/bin:$PATH"
+# [ -d $HOME/local-packages-repository ] and set PATH $HOME/local-packages-repository/bin $PATH
 
 ## linuxbrew path
-# set PATH "$HOME/.linuxbrew/bin:$PATH"
-# set MANPATH "$HOME/.linuxbrew/share/man:$MANPATH"
-# set INFOPATH "$HOME/.linuxbrew/share/info:$INFOPATH"
-# set LD_LIBRARY_PATH "$HOME/.linuxbrew/lib:$LD_LIBRARY_PATH"
+set PATH $HOME/.linuxbrew/bin $PATH
+# set MANPATH $HOME/.linuxbrew/share/man $MANPATH
+# set INFOPATH $HOME/.linuxbrew/share/info $INFOPATH
+# set LD_LIBRARY_PATH $HOME/.linuxbrew/lib $LD_LIBRARY_PATH
 # if [ -f $(brew --prefix)/etc/brew-wrap ];then
 #   source $(brew --prefix)/etc/brew-wrap
 # fi
@@ -29,14 +29,14 @@ end
 
 ## goenv path
 # if [ -d $HOME/.goenv ] ; then
-#   set PATH "$HOME/.goenv/bin:$PATH"
+#   set PATH $HOME/.goenv/bin $PATH
 #   eval "$(goenv init -)"
 # fi
 
 ## go path
 # if type go > /dev/null 2>&1; then
-#   set GOPATH "$HOME/gocode"
-#   PATH="$GOPATH/bin:$PATH"
+#   set GOPATH $HOME/gocode
+#   PATH=$GOPATH/bin $PATH
 # fi
 
 ## nvm path
