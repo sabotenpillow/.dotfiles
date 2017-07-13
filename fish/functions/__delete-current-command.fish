e167ecc6ef98e816f3a274984c503a223777d2ac
sabotenpillow <saboten.pillow@gmail.com>
2017-07-13 05:04:15 +0000

    __delete-current-command.fish

--- fish/functions/__delete-current-command.fish
+++ fish/functions/__delete-current-command.fish
@@ -1,14 +1,6 @@
 function __delete-current-command
+  set -l get_commandline (commandline)
+  set -l arguments ' ' (string split --max 1 ' ' $get_commandline)[2]
+  commandline -r (string join '' $arguments)
   commandline -f beginning-of-line
-  commandline -f kill-bigword
-  # commandline -f forward-char
-  # set -l get_token (commandline -t)
-  # echo (string length $get_token)
-  # echo
-  # echo $get_token
-  # if [ (string length $get_token) = 1 ];
-  #   # commandline -f delete-char
-  # else
-  #   commandline -f kill-bigword
-  # end
 end
